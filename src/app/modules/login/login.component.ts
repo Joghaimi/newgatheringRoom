6d22c60ba9b57b855f953033f48f654732b0cf43
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-01-26 20:05:11 +0300

    commit

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { interval, switchMap } from 'rxjs';
 import { Player } from 'src/app/models/player';
 import { TeamService } from 'src/app/services/TeamService';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,14 @@ import { TeamService } from 'src/app/services/TeamService';
 })
 export class LoginComponent {
   title = 'GatheringRoom';
+  teamName = "";
   players: Player[] = [
-    { uid: "123456", firstName: "Ahmad", secoundName: "said" },
-    { uid: "123456", firstName: "Mohammad", secoundName: "said" },
-    { uid: "123456", firstName: "hassan", secoundName: "said" },
-    { uid: "123456", firstName: "Tariq", secoundName: "said" },
+    // { uid: "123456", firstName: "Ahmad", secoundName: "said" },
+    // { uid: "123456", firstName: "Mohammad", secoundName: "said" },
+    // { uid: "123456", firstName: "hassan", secoundName: "said" },
+    // { uid: "123456", firstName: "Tariq", secoundName: "said" },
   ];
+
   constructor(private teamService: TeamService) {
 
   }
@@ -26,24 +29,30 @@ export class LoginComponent {
         switchMap(() => this.teamService.getTeamMembers())
       )
       .subscribe(response => {
-        let newplayers :Player[]= [];
+        let newplayers: Player[] = [];
         response.forEach(
           e => {
-            let newPlayer:Player = {
-              uid:e,
-              firstName:e,
-              secoundName:e
+            let newPlayer: Player = {
+              uid: e,
+              firstName: e,
+              secoundName: e
 
             };
             newplayers.push(newPlayer);
 
           }
         );
-        this.players=newplayers;
+        this.players = newplayers;
         // Handle the response here
         console.log(response);
       });
   }
   // init Form 
+  GoToTheNextRoom() {
+    this.teamService.goToTheNextRoom().subscribe(
+
+    );
+
+  }
 
 }
--- src/app/services/TeamService.ts
+++ src/app/services/TeamService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,11 @@ import {Observable} from 'rxjs';
 export class TeamService {
 
   constructor(private httpClient: HttpClient) { }
-  getTeamMembers():Observable<any[]> {
+  getTeamMembers(): Observable<any[]> {
     console.log("Send")
     return this.httpClient.get<any[]>('https://localhost:7248/GatheringRoom');
+  }
+  goToTheNextRoom() {
+    return this.httpClient.get('https://localhost:7248/GoToTheNextRoom');
   }
 }
