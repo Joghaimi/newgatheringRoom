4ae3b5d749ae08cfd60b1a443ed25c5c65b60ddb
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-24 21:39:11 +0300

    fix overlay

--- src/app/modules/joinning-page/joinning-page.component.ts
+++ src/app/modules/joinning-page/joinning-page.component.ts
@@ -23,7 +23,10 @@ export class JoinningPageComponent {
   title = 'GatheringRoom';
   teamName = "Enter Your Team Name";
   players: Player[] = [
-
+    {firstName:"ahmad", lastName:"said"},
+    {firstName:"ahmad", lastName:"said"},
+    {firstName:"ahmad", lastName:"said"},
+    {firstName:"ahmad", lastName:"said"},
   ];
 
   constructor(private teamService: TeamService) {
