578a33a576d3a0a4601116905def295168fd116f
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-01 22:42:32 +0300

    cc

--- src/app/modules/dark-room/dark-room.component.ts
+++ src/app/modules/dark-room/dark-room.component.ts
@@ -20,7 +20,7 @@ export class DarkRoomComponent {
   nextGame = "floor";
   nextGame2 = "floorislava";
   score = 0;
-  gameTotalTime = 120;
+  gameTotalTime = 540;
   team: Team = { name: "Team Name" };
   countdownSubscription!: Subscription;
 
@@ -43,7 +43,7 @@ export class DarkRoomComponent {
   }
   GoToTheNextRoom() {
     // Restart The Game
-    this.gameTotalTime = 120;
+    this.gameTotalTime = 540;
     this.goToTheNextRoom = false;
     this.showLoading=true;
     let interval = setInterval(() => {
